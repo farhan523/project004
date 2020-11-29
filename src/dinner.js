2107f2455f760855e292c983cbd2744a942ea15c
farhanBajwa <62025759+farhan523@users.noreply.github.com>
2020-11-29 23:51:39 +0500

    Create dinner.js

---
+++ src/dinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dinner(props){
+    return(
+        <div>
+            <h1>today we are surving {props.dishName}</h1>
+        </div>
+    )
+}
+
+export default Dinner;
