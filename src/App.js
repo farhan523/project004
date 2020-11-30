79ab8521db473ee3b4b2afc7eb7d73e36b5a2f9f
farhanBajwa <62025759+farhan523@users.noreply.github.com>
2020-11-30 12:47:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-
-import Dinner from './dinner.js'
+import Dinner from './dinner.js';
 
 function App() {
     return(
-      <div className="App">
-        <header className="App.header">
-          <h1>hello good evenings</h1>
-        </header>
+      <div>
+        <Dinner />
       </div>
     );
 }
